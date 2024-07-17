d28a1923b52b6889d06083d329a8ce2bbb75f402
DeveloperIlhombek <ilxomdeveloper@gmail.com>
2024-07-17 18:15:40 +0500

    add all pages

--- types/index.ts
+++ types/index.ts
@@ -1,3 +1,15 @@
 export interface ChildProps {
 	children: React.ReactNode
 }
+export interface IBlog {
+	title: string
+	description: string
+	author: string
+	data: string
+	image: string
+	tags: string[]
+}
+export interface IAuthor {
+	Image: string
+	name: string
+}
