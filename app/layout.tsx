e3658dcef28b133fd0cdae408d2f60be68695b53
DeveloperIlhombek <ilxomdeveloper@gmail.com>
2024-07-22 16:34:13 +0500

    top loader &  media link

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Toaster } from '@/components/ui/sonner'
 import { ChildProps } from '@/types'
 import type { Metadata } from 'next'
 import { Crete_Round, Work_Sans } from 'next/font/google'
+import NextTopLoader from 'nextjs-toploader'
 import './globals.css'
 
 const creteRound = Crete_Round({
@@ -50,6 +51,7 @@ function RootLayout({ children }: ChildProps) {
 					enableSystem
 					disableTransitionOnChange
 				>
+					<NextTopLoader />
 					{children}
 					<Toaster position='top-center' />
 				</ThemeProvider>
